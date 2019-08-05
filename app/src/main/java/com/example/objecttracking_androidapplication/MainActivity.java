7a1143657f3e0271e644c8ee21d21837e108f5ab
Clair <zhiqing.clair.hu@gmail.com>
2019-08-05 15:36:47 -0700

    "Restart."

---
+++ app/src/main/java/com/example/objecttracking_androidapplication/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.objecttracking_androidapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
